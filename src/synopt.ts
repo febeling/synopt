405f06a24dcafd9f0006fc394eaa43982013f766
Florian Ebeling <mail@florianebeling.com>
2023-12-06 17:48:41 +0100

    More typeings

--- src/synopt.ts
+++ src/synopt.ts
@@ -1,56 +1,98 @@
-/** 
+
+interface Command {
+  summary: (text: string) => Command;
+  description: (text: string) => Command;
+  option: (...args: DeclarationTuple) => Command;
+  parse: (args: string[]) => object;
+  declarations: () => OptionDeclaration[];
+  usage: () => string;
+}
+
+interface OptionDeclaration {
+  boolean?: boolean;
+  short?: string;
+  argname?: string;
+  long?: string;
+  name?: string;
+  description?: string;
+}
+
+interface CommandState {
+  name?: string;
+  optionDeclarations: OptionDeclaration[];
+  summary?: string;
+  description?: string;
+}
+
+interface DeclarationOptions {
+  boolean?: boolean;
+}
+
+type DeclarationTuple = [
+  name: string,
+  alias?: string,
+  description?: string,
+  options?: OptionDeclaration
+];
+
+/**
  * Parse an option declaration list into an argument list parser
  *
  * The object representing the option interface also parses an argument vector into an concrete options object.
  */
 
-const parseDeclaration = (arr) => {
-  const decl = arr.slice().reverse();
+const parseDeclaration = (declaration: DeclarationTuple) => {
+  const decl = declaration.slice().reverse();
 
-  const option: any = {};
+  const option: OptionDeclaration = {};
 
   const reLong = /^(--([-\w]+?))( (\w+?)?)?$/;
   const reShort = /^-[^-]$/;
 
   while (decl.length > 0) {
     const elem = decl.pop();
-    if (decl.length === 0 && typeof (elem) === 'object') {
-      if (elem.boolean === true) {
+    if (decl.length === 0 && typeof elem === "object") {
+      if ((elem as DeclarationOptions).boolean === true) {
         option.boolean = true;
       }
-    } else if (reShort.test(elem)) {
-      option.short = elem;
-    } else if (reLong.test(elem)) {
-      const [_all, long, name, _x, argname] = elem.match(reLong);
-      option.long = long;
-      option.argname = argname || name.toUpperCase();
-      option.name = name;
-    } else if (option.name && !option.description) {
-      option.description = elem;
     } else {
-      throw new Error(`parse error: ${arr}`);
+      const str = elem as string;
+      if (reShort.test(str)) {
+        option.short = str;
+      } else if (reLong.test(str)) {
+        const [_all, long, name, _x, argname] = str.match(reLong);
+        option.long = long;
+        option.argname = argname || name.toUpperCase();
+        option.name = name;
+      } else if (option.name && !option.description) {
+        option.description = str;
+      } else {
+        throw new Error(`parse error: ${declaration}`);
+      }
     }
   }
+
   if (!option.name || !option.long) {
-    throw new Error('option name required');
+    throw new Error("option name required");
   }
+
   return option;
 };
 
-const isOption = (string) => /^-/.test(string);
+const isOption = string => /^-/.test(string);
 
-const createCommand = (name?: string) => {
-  const state: any = {
+const createCommand = (name?: string): Command => {
+  const state: CommandState = {
     name,
     optionDeclarations: [],
   };
 
-  const command = {
-    summary: (text) => {
+  const command: Command = {
+    summary: text => {
       state.summary = text;
       return command;
     },
-    description: (text) => {
+    description: text => {
       state.description = text;
       return command;
     },
@@ -68,7 +110,7 @@ const createCommand = (name?: string) => {
       state.optionDeclarations.push(declaration);
       return command;
     },
-    parse: (args) => {
+    parse: args => {
       const options = {};
 
       for (let i = 0; i < args.length; i++) {
@@ -86,7 +128,9 @@ const createCommand = (name?: string) => {
           options[elementDecl.name] = nextElement;
           i++;
         } else {
-          throw new Error(`option '${element}' requires value, because it's not boolean`);
+          throw new Error(
+            `option '${element}' requires value, because it's not boolean`
+          );
         }
       }
 
@@ -97,30 +141,38 @@ const createCommand = (name?: string) => {
       const shorts = state.optionDeclarations.map((val, i) => val.short);
       const longs = state.optionDeclarations.map((val, i) => val.long);
       const argnames = state.optionDeclarations.map((val, i) => val.argname);
-      const descriptions = state.optionDeclarations.map((val, i) => val.description);
+      const descriptions = state.optionDeclarations.map(
+        (val, i) => val.description
+      );
 
-      const longMax = Math.max(...longs.map((l, i) => {
-        let len = l.length;
-        if (!state.optionDeclarations[i].boolean) {
-          len += argnames[i].length + 1;
-        }
-        return len;
-      }));
+      const longMax = Math.max(
+        ...longs.map((l, i) => {
+          let len = l.length;
+          if (!state.optionDeclarations[i].boolean) {
+            len += argnames[i].length + 1;
+          }
+          return len;
+        })
+      );
 
       const anyShorts = shorts.filter(x => x).length > 0;
 
       const optionHelpLines = longs.map((long, i) => {
-        return `    ${anyShorts ? (shorts[i] ? shorts[i] + ',' : '   ') : ''}  ${(long + (state.optionDeclarations[i].boolean ? '' : ` ${argnames[i]}`)).padEnd(longMax, ' ')}  ${descriptions[i]}`;
+        return `    ${
+          anyShorts ? (shorts[i] ? shorts[i] + "," : "   ") : ""
+        }  ${(
+          long + (state.optionDeclarations[i].boolean ? "" : ` ${argnames[i]}`)
+        ).padEnd(longMax, " ")}  ${descriptions[i]}`;
       });
 
       return [
-        `Usage: ${state.name || 'SCRIPT_NAME'} [options]`,
-        ...(!!state.summary ? ['', state.summary] : []),
-        ...(!!state.description ? ['', state.description] : []),
-        '',
-        ...optionHelpLines
+        `Usage: ${state.name || "SCRIPT_NAME"} [options]`,
+        ...(!!state.summary ? ["", state.summary] : []),
+        ...(!!state.description ? ["", state.description] : []),
+        "",
+        ...optionHelpLines,
       ].join("\n");
-    }
+    },
   };
   return command;
 };
