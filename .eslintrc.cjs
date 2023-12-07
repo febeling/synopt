9431750ef5d96b6b5168a5e34018e92c12fac9ff
Florian Ebeling <mail@florianebeling.com>
2023-12-07 17:31:55 +0100

    wip prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,6 +3,5 @@ module.exports = {
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
-  root: true,
-  ignorePatterns: ["dist"]
+  root: true
 };
